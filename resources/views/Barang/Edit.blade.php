6223235d3743ddd3bc823ddb5227bbc6047dda10
selaaulia <selaaulia1307@gmail.com>
2021-03-30 23:09:16 +0700

    Menampilkan halaman edit untuk update data

--- resources/views/Barang/Edit.blade.php
+++ resources/views/Barang/Edit.blade.php
@@ -20,38 +20,38 @@
                             </ul>
                         </div>
                     @endif
-                    <form method="post" action="{{ route('barang.update', $Barang->id_barang) }}" id="myForm">
+                    <form method="post" action="{{ route('barang.update', $barangs->id_barang) }}" id="myForm">
                         @csrf
                         @method('PUT')
                         <div class="form-group">
                             <label for="id_barang">id_barang</label>
-                            <input type="text" name="id_barang" class="form-control" id="id_barang" value="{{ $Barang->id_barang }}"
+                            <input type="text" name="id_barang" class="form-control" id="id_barang" value="{{ $barangs->id_barang }}"
                                 aria-describedby="id_barang">
                         </div>
                         <div class="form-group">
                             <label for="kode_barang">kode_barang</label>
-                            <input type="text" name="kode_barang" class="form-control" id="kode_barang" value="{{ $Barang->kode_barang }}"
+                            <input type="text" name="kode_barang" class="form-control" id="kode_barang" value="{{ $barangs->kode_barang }}"
                                 aria-describedby="kode_barang">
                         </div>
                         <div class="form-group">
                             <label for="nama_barang">nama_barangs</label>
                             <input type="nama_barang" name="nama_barang" class="form-control" id="nama_barang"
-                                value="{{ $Barang->nama_barang }}" aria-describedby="nama_barang">
+                                value="{{ $barangs->nama_barang }}" aria-describedby="nama_barang">
                         </div>
                         <div class="form-group">
                             <label for="kategori_barang">kategori_barang</label>
                             <input type="kategori_barang" name="kategori_barang" class="form-control" id="kategori_barang"
-                                value="{{ $Barang->kategori_barang }}" aria-describedby="kategori_barang">
+                                value="{{ $barangs->kategori_barang }}" aria-describedby="kategori_barang">
                         </div>
                         <div class="form-group">
                             <label for="harga">harga</label>
                             <input type="harga" name="harga" class="form-control" id="harga"
-                                value="{{ $Barang->harga }}" aria-describedby="harga" placeholder="Year-Month-Day">
+                                value="{{ $barangs->harga }}" aria-describedby="harga" placeholder="Year-Month-Day">
                         </div>
                         <div class="form-group">
                             <label for="qty">qty</label>
                             <input type="qty" name="qty" class="form-control" id="qty"
-                                value="{{ $Barang->qty }}" aria-describedby="qty">
+                                value="{{ $barangs->qty }}" aria-describedby="qty">
                         </div>
                         <button type="submit" class="btn btn-primary">Submit</button>
                     </form>
