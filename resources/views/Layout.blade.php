a6591bd280fb424bd242f29280bdcd96af1f4c47
selaaulia <selaaulia1307@gmail.com>
2021-03-30 16:17:26 +0700

    Modifikasi Layout pada bagian judul

--- resources/views/Layout.blade.php
+++ resources/views/Layout.blade.php
@@ -1,6 +1,6 @@
 <html>
  <head>
- <title>Sistem Informasi Mahasiswa</title>
+ <title>TOKO SERBA ADA</title>
  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap
 /4.4.1/css/bootstrap.min.css" integrity="sha384-
 Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="an
