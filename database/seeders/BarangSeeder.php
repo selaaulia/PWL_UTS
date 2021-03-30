c26561d29b1062ae3007b18f391acf779e0f0d35
selaaulia <selaaulia1307@gmail.com>
2021-03-30 16:24:26 +0700

    Membuat Index barang dan modifikasi BarangSeeder

--- database/seeders/BarangSeeder.php
+++ database/seeders/BarangSeeder.php
@@ -15,25 +15,33 @@ class BarangSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('barangs')->insert([
-            'kode_barang'=>['PRD001', 'PRD002', 'PRD003', 'PRD004', 'PRD005',
+            $kode_barang =['PRD001', 'PRD002', 'PRD003', 'PRD004', 'PRD005',
                             'PRD006', 'PRD007', 'PRD008', 'PRD009', 'PRD010',
                             'PRD011', 'PRD012', 'PRD013', 'PRD014', 'PRD015',
-                            'PRD016', 'PRD017', 'PRD018', 'PRD019', 'PRD020'],
-            'nama_barang'=>['Indomie', 'Pocari Sweat', 'Silverqueen', 'Potabee', 'Aqua',
+                            'PRD016', 'PRD017', 'PRD018', 'PRD019', 'PRD020'];
+            $nama_barang =['Indomie', 'Pocari Sweat', 'Silverqueen', 'Potabee', 'Aqua',
                             'Mie Sedaap', 'Sari Roti', 'Lemonilo', 'Sarimi', 'Paroti',
                             'Teh Pucuk', 'UC1000', 'Cimori', 'Conetto', 'Magnum',
-                            'Citato', 'Cheetos', 'Pringles', 'Lays', 'Soyjoy'],
-            'kategori_barang'=>['Makanan','Minuman', 'Snack', 'Snack', 'Minuman',
+                            'Citato', 'Cheetos', 'Pringles', 'Lays', 'Soyjoy'];
+            $kategori_barang =['Makanan','Minuman', 'Snack', 'Snack', 'Minuman',
                                 'Makanan', 'Makanan', 'Makanan', 'Makanan', 'Makanan',
                                 'Minuman', 'Minuman', 'Minuman', 'Es Krim', 'Es Krim', 
-                                'Snack', 'Snack', 'Snack', 'Snack', 'Snack'],
-            'harga'=>['2500', '6000', '25000', '7500', '2500',
+                                'Snack', 'Snack', 'Snack', 'Snack', 'Snack'];
+            $harga =['2500', '6000', '25000', '7500', '2500',
                     '2500', '4500', '4500', '3000', '4000',
                     '3500', '7000', '10000', '15000', '15000',
-                    '7000', '9000', '12000', '1100', '7500'],
-            'qty'=>['25', '10', '11', '5', '10', '7', '8', '9', '10', '12', '13', '15', '17', '19', '20', '23', '25', '35', '18', '20']
-        ]);
+                    '7000', '9000', '12000', '1100', '7500'];
+            $qty =['25', '10', '11', '5', '10', '7', '8', '9', '10', '12', '13', '15', '17', '19', '20', '23', '25', '35', '18', '20'];
 
+        for($i=0; $i < 20; $i++){
+            DB::table('barangs')->insert([
+                //'id_barang'=> $id_barang[$i],
+                'kode_barang' => $kode_barang[$i],
+                'nama_barang' => $nama_barang[$i],
+                'kategori_barang' => $kategori_barang[$i],
+                'harga' => $harga[$i],
+                'qty' => $qty[$i]
+            ]);
+        }
     }
 }
