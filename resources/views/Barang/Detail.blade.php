978b796505a8fb7e0d7f71587f59890a2c4de1a3
selaaulia <selaaulia1307@gmail.com>
2021-03-30 23:07:58 +0700

    Menampilkan detail-detail infromasi barang

--- resources/views/Barang/Detail.blade.php
+++ resources/views/Barang/Detail.blade.php
@@ -10,12 +10,12 @@
                 </div>
                 <div class="card-body">
                     <ul class="list-group list-group-flush">
-                        <li class="list-group-item"><b>id_barang: </b>{{ $Barang->id_barang }}</li>
-                        <li class="list-group-item"><b>kode_barang: </b>{{ $Barang->kode_barang }}</li>
-                        <li class="list-group-item"><b>nama_barang: </b>{{ $Barang->nama_barang }}</li>
-                        <li class="list-group-item"><b>kategori_barang: </b>{{ $Barang->kategori_barang }}</li>
-                        <li class="list-group-item"><b>harga: </b>{{ $Barang->harga }}</li>
-                        <li class="list-group-item"><b>qty: </b>{{ $Barang->qty }}</li>
+                        <li class="list-group-item"><b>id_barang: </b>{{ $barangs->id_barang }}</li>
+                        <li class="list-group-item"><b>kode_barang: </b>{{ $barangs->kode_barang }}</li>
+                        <li class="list-group-item"><b>nama_barang: </b>{{ $barangs->nama_barang }}</li>
+                        <li class="list-group-item"><b>kategori_barang: </b>{{ $barangs->kategori_barang }}</li>
+                        <li class="list-group-item"><b>harga: </b>{{ $barangs->harga }}</li>
+                        <li class="list-group-item"><b>qty: </b>{{ $barangs->qty }}</li>
                     </ul>
                 </div>
                 <a class="btn btn-success mt-3" href="{{ route('barang.index') }}">Kembali</a>
